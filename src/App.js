ad71ebada4b39834a4bccb766a352af0b2c7ce47
annykashyap <anjalikashyapakk@gmail.com>
2024-09-10 21:23:21 +0530

    added searching functionalities

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import { useState } from "react";
+import axios from "axios";
+
 function App() {
-  return <div className="App"></div>;
+  let ACCESS_API = "wZFkoHuCydOZlTBFSO3ARGqfKRkonVzdCF2lwx4wDkA";
+  const [getinput, setinput] = useState();
+  const [data, setdata] = useState([]);
+
+  const handlechange = (e) => {
+    setinput(e.target.value);
+  };
+  const handlsearch = async (e) => {
+    const response = await axios.get(
+      `https://api.unsplash.com/search/photos?page=1&query=${getinput}&client_id=${ACCESS_API}`
+    );
+
+    setdata(response.data.results);
+  };
+
+  return (
+    <div className="App">
+      <div>
+        <input type="text" value={getinput} onChange={handlechange} />
+        <button onClick={handlsearch}>search</button>
+      </div>
+      {data.map((item) => {
+        return (
+          <>
+            <h1>{item.titles}</h1>
+            <img src={item.urls.small} />
+          </>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
